7f75a44dbda718623eabac0fe2eb29ba9f3362ca
vivek <vivek_rajagopal@yahoo.com>
2014-04-02 19:18:28 +0530

    TCP Header printer ..

--- lua/buffer/tcphdr.lua
+++ lua/buffer/tcphdr.lua
@@ -34,7 +34,19 @@ TrisulPlugin = {
 			print("Dest port   = " .. buff:hval_16(2))
 			print("Seq number  = " .. buff:hval_32(4))
 			print("Ack number  = " .. buff:hval_32(8))
+                        local fval= buff:hval_16(12)
 
+			-- how to extract a single bit 
+			print("RST flag    = " .. bit32.extract(fval,2))
+
+			-- print all the flags 
+                        print("flags")
+                        local t={[8]="Nonce",[7]="CWR",[6]="ECN-Echo",[5]="Urgent",[4]="Acknowledgement",[3]="Push",[2]="Reset",[1]="Syn",[0]="Fin"}
+                        for k,v in pairs(t) do 
+				local bit_state =  bit32.extract(fval,k)==0 and "(0) Not Set" or "(1) Set"
+                        	print("\t"..v..":"..bit_state)
+                        end
+                        print(string.rep("_",100))
 
 
 		end,
@@ -44,3 +56,4 @@ TrisulPlugin = {
 
 
 }
+
