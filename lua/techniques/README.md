cffad5c57c5c8e8323e75f397bda483fb752359e
vivek <vivekrjg@gmail.com>
2014-04-07 21:31:47 +0530

    New HTTP logger - Part 2

--- lua/techniques/README.md
+++ lua/techniques/README.md
@@ -8,7 +8,7 @@ Demonstrates LUA scripting techniques
 1. re2http.lua
    --------------
 
-   A fully customizable HTTP logger.
+   Log HTTP requests and responses
 
    Demonstates the use of T.re2(). Trisul exports the Google RE2 Regex Library 
    it uses internally to the LUA side. The expressions are precompiled in the 
@@ -20,4 +20,26 @@ Demonstrates LUA scripting techniques
 
    partial_match_c2( ) matches a regex with two captures
 
-   
+   Next steps : Correlate requests and responses
+
+  
+2. httplog.lua
+   -----------
+
+   A fully customizable request-response correlating HTTP logger
+
+   The re2http.lua sample logged requests and responses on separate lines. This scripts
+   uses the flow:id() method to correlate responses to requests and log them on a single
+   line.
+  
+   Also demonstrates the following
+
+   - how you can "include" a Lua file such as queue.lua 
+
+   - opening output log files in a threadsafe way by adding random number
+
+   - timestamping, working with queues, and more
+
+   Next steps : Listen to flow termination event to clean up map and okay garbage collection.
+
+
---
+++ lua/techniques/httplog.lua
@@ -0,0 +1,116 @@
+-- httplog.lua
+--
+-- A customizable HTTP logger, correlates requests and responses 
+-- 
+--
+
+require("queue")
+
+TrisulPlugin = {
+
+  id = {
+    name = "HTTP Logger ",
+    description = "Log req/resp in one line ",
+    author = "Unleash", version_major = 1,
+    version_minor = 0,
+  },
+
+
+
+  onload = function () 
+
+    P = TrisulPlugin
+
+	P.regex = {
+
+		requests = {
+			T.re2("(.*)HTTP/1.\\d\r\n"),
+			T.re2("Host\\s*:\\s*(.*)\r\n"),
+			T.re2("User-Agent\\s*:\\s*(.*)\r\n"),
+			T.re2("Referer\\s*:\\s*(.*)\r\n")
+		},
+
+		responses = {
+			T.re2("HTTP/1.\\d\\s*(.*)\r\n"),
+			T.re2("Server\\s*:\\s*(.*)\r\n"),
+			T.re2("Content-Type\\s*:\\s*(.*)\r\n"),
+			T.re2("Content-Length\\s*:\\s*(.*)\r\n"),
+		}
+
+	}
+
+
+	P.flowmap = { } 
+
+	P.outfile = io.open("/tmp/httpheaders-"..math.random(1000,2000)..".log","w")
+
+  end,
+
+
+  onunload = function()
+
+  	P.outfile.close()
+
+  end,
+
+
+  flowmonitor  = {
+
+	onflowattribute = function(engine,flow,timestamp,
+							   nm, val)
+
+	     if nm == "HTTP-Header" then
+
+		 	local flowkey = flow:id()
+			P.flowmap[flowkey] = P.flowmap[flowkey] or queue.new()
+			local q = P.flowmap[flowkey]
+
+		 	if val:find("^HTTP/") then
+
+				local a =  q:popfirst()
+
+
+
+				-- response 
+				for i,v in ipairs( P.regex.responses) do 
+					local status, match = v:partial_match_c1(val)
+					if status then a[#a+1] = match end
+				end
+				
+
+				-- log everything 
+				P.outfile:write(os.date("%c ",timestamp))
+				P.outfile:write(flow:ipa_readable())
+				P.outfile:write(" ")
+				P.outfile:write(flow:ipz_readable())
+				P.outfile:write(" ")
+				for i,v in ipairs(a) do
+					P.outfile:write(v)
+					P.outfile:write(" ")
+				end
+				P.outfile:write("\n")
+
+			else
+
+				local a = { } 
+
+				-- request 
+				for i,v in ipairs( P.regex.requests) do 
+					local status, match = v:partial_match_c1(val)
+					if status then a[i] = match end
+				end
+
+				q:pushlast(a) 
+
+			end
+
+
+
+			
+		 end
+
+    end,
+
+  },
+
+}
