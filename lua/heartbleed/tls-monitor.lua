b6be181f76e949408fc4f17538dffa7b79d314a1
vivek <vivek_rajagopal@yahoo.com>
2014-04-10 19:14:11 +0530

    Tls monitor content-type + handshake-type

--- lua/heartbleed/tls-monitor.lua
+++ lua/heartbleed/tls-monitor.lua
@@ -24,6 +24,28 @@ TrisulPlugin = {
         {  0, T.K.vartype.COUNTER,	 10, "Hits", "Hits",    "hits" },
     },  
 
+	keyinfo = {
+		{"14/00","change_cipher_spec"},
+		{"15/00","alert"},
+		{"16/00","hello_request"},
+		{"16/01","client_hello"},
+		{"16/02","server_hello"},
+		{"16/03","hello_verify_request"},
+		{"16/04","NewSessionTicket"},
+		{"16/0B","certificate"},
+		{"16/0C","server_key_exchange"},
+		{"16/0D","certificate_request"},
+		{"16/0E","server_hello_done"},
+		{"16/0F","certificate_verify"},
+		{"16/10","client_key_exchange"},
+		{"16/14","finished"},
+		{"16/15","certificate_url"},
+		{"16/16","certificate_status"},
+		{"16/17","supplemental_data"},
+		{"17/00","application_data"},
+		{"18/00","heartbeat"},
+	}
+
   },
 
  
@@ -33,11 +55,22 @@ TrisulPlugin = {
 
 	onflowattribute = function(engine,flow,timestamp, nm, valbuff)
 
-
 	     if nm == "TLS:RECORD" then
 		 	local content_type = valbuff:hval_8(0)
+			local handshake_type = 0
 
-			local keystr = string.format("%02x", content_type )
+			if content_type == 22 then
+				-- heuristic check if encrypted 
+				handshake_type = valbuff:hval_8(5)
+				local hslen = valbuff:hval_24(6)
+				if hslen > valbuff:size() or  handshake_type >= 24  then 
+					handshake_type=0
+				end
+
+			end
+
+
+			local keystr = string.format("%02x/%02X", content_type , handshake_type)
 
 			engine:update_counter("{fc970d3a-5a39-4e31-a687-672c5174a58e}", 
 						keystr, 0, 1)
