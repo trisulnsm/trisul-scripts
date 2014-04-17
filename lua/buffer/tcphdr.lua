38608fc5ef598b66922e4b0cbb49bdbc901c906e
vivek <vivek_rajagopal@yahoo.com>
2014-04-17 13:56:30 +0530

    Aligned with LuaJIT 5.1

--- lua/buffer/tcphdr.lua
+++ lua/buffer/tcphdr.lua
@@ -1,58 +1,64 @@
 --
 -- tcphdr.lua
 --
--- 	Prints the TCP header  - does not actually meter anything 
+--  Prints the TCP headers to stdout - does not actually meter anything 
 --
--- 	Demonstrates how you can work with 
--- 	 1) The Buffer object
--- 	 2) The Layer
--- 	 3) The Packet 
+--  Demonstrates how you can work with 
+--   1) The Buffer object
+--   2) The Layer
+--   3) The Packet 
 --
 TrisulPlugin = {
 
-	id = {
-		name = "TCPHDR ",
-		description = "Demo - prints tcp header ",
-		author = "Unleash",
-		version_major = 1,
-		version_minor = 0,
-	},
+  id = {
+    name = "TCPHDR ",
+    description = "Demo - prints tcp header ",
+    author = "Unleash",
+    version_major = 1,
+    version_minor = 0,
+  },
 
-	simplecounter = {
+  simplecounter = {
 
-		protocol_guid = "{77E462AB-2E42-42ec-9A58-C1A6821D6B31}",
+    -- attach to the TCP protocol http://trisul.org/docs/ref/guid.html#protocols
+    protocol_guid = "{77E462AB-2E42-42ec-9A58-C1A6821D6B31}",
 
-		-- onpacket
-		onpacket = function(engine,layer)
+    -- onpacket
+    onpacket = function(engine,layer)
 
-			local buff = layer:rawbytes() 
+      local buff = layer:rawbytes() 
 
-			-- T.debugger({ engine = engine, layer = layer })
-			--
-			
-			print("Source port = " .. buff:hval_16(0))
-			print("Dest port   = " .. buff:hval_16(2))
-			print("Seq number  = " .. buff:hval_32(4))
-			print("Ack number  = " .. buff:hval_32(8))
-                        local fval= buff:hval_16(12)
+      -- T.debugger({ engine = engine, layer = layer })
+      --
+      
+      print("Source port = " .. buff:hval_16(0))
+      print("Dest port   = " .. buff:hval_16(2))
+      print("Seq number  = " .. buff:hval_32(4))
+      print("Ack number  = " .. buff:hval_32(8))
 
-			-- how to extract a single bit 
-			print("RST flag    = " .. bit32.extract(fval,2))
+      local fval= buff:hval_16(12)
 
-			-- print all the flags 
-                        print("flags")
-                        local t={[8]="Nonce",[7]="CWR",[6]="ECN-Echo",[5]="Urgent",[4]="Acknowledgement",[3]="Push",[2]="Reset",[1]="Syn",[0]="Fin"}
-                        for k,v in pairs(t) do 
-				local bit_state =  bit32.extract(fval,k)==0 and "(0) Not Set" or "(1) Set"
-                        	print("\t"..v..":"..bit_state)
-                        end
-                        print(string.rep("_",100))
+      -- how to extract a single bit ]
+      -- since we are using LuaJIT we cant use the new bit32 library in Lua5.2
+      --
+      print("RST flag    = " .. tostring(T.util.testbit32(fval,2)))
+      print("SYN flag    = " .. tostring(T.util.testbit32(fval,1)))
+      print("FIN flag    = " .. tostring(T.util.testbit32(fval,0)))
+
+      -- can also test for a single bit from the layer object directly
+      -- for example RST is bit number 109 in the TCP header
+      print("RST flag alt   = " .. tostring(layer:testbit(109)))
+
+      -- get 4 bit frame offset 
+      print("Frame offset words  = " .. T.util.bitval32(layer:getbyte(12),7,4))
+	  -- or from the buffer
+      print("Frame offset words = " .. T.util.bitval32(buff:hval_8(12),7,4))
 
 
-		end,
+    end,
 
 
-	 },
+   },
 
 
 }
