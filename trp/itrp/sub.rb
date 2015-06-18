2c3a0642702ae39007431618d1aeb01029d43b88
vivekrajan <vivek@unleashnetworks.com>
2015-06-18 18:37:39 +0530

    iSub raw counters

--- trp/itrp/sub.rb
+++ trp/itrp/sub.rb
@@ -6,10 +6,6 @@
 #
 #
 require 'trisulrp'
-require 'readline'
-require 'rb-readline'
-require 'terminal-table'
-require 'matrix'
 
 # Check arguments
 raise %q{
@@ -17,16 +13,17 @@ raise %q{
 
   pubsub.rb - test the pubsub interface 
 
-  Usage   : pubsub.rb  trisul-zmq-endpt-sub  [addcg delcg addids delids addtop deltop addflow ] 
-  Example : pubsub.rb  tcp://192.168.1.8:5555 addcg 
+  Usage   : sub.rb  trisul-zmq-endpt-sub  [addcg delcg addids delids addtop deltop addflow aggcgraw delcgraw]
+  Example : sub.rb  tcp://192.168.1.8:5555 addcg 
 
-} unless ARGV.length==2
+} unless ARGV.length>=2
 
 
 # parameters 
 #
 zmq_endpt_sub   = ARGV.shift
 cmd   = ARGV.shift
+
 
 zmq_ctx = ZMQ::Context.new
 
@@ -35,45 +32,63 @@ zmq_ctx = ZMQ::Context.new
 s1 = zmq_ctx.socket(ZMQ::PUSH)
 s1.connect(zmq_endpt_sub)
 
-if cmd=="addcg" 
-	req =mk_request(TRP::Message::Command::STAB_PUBSUB_CTL,
+
+req= case cmd
+
+when  "addcg"
+	mk_request(TRP::Message::Command::STAB_PUBSUB_CTL,
 					 :ctl => TRP::SubscribeCtl::CtlType::CT_SUBSCRIBE,
 					 :type => TRP::SubscribeCtl::StabberType::ST_COUNTER_ITEM,
 					 :guid => TrisulRP::Guids::CG_HOST,
 					 :key => "C0.A8.02.08")
-elsif cmd=="delcg"
+when "delcg"
 	req =mk_request(TRP::Message::Command::STAB_PUBSUB_CTL,
 					 :ctl => TRP::SubscribeCtl::CtlType::CT_UNSUBSCRIBE,
 					 :type => TRP::SubscribeCtl::StabberType::ST_COUNTER_ITEM,
 					 :guid => TrisulRP::Guids::CG_HOST,
 					 :key => "C0.A8.02.08")
-elsif cmd=="addids"
+when "addids"
 	req =mk_request(TRP::Message::Command::STAB_PUBSUB_CTL,
 					 :ctl => TRP::SubscribeCtl::CtlType::CT_SUBSCRIBE,
 					 :type => TRP::SubscribeCtl::StabberType::ST_ALERT,
 					 :guid => TrisulRP::Guids::AG_IDS)
-elsif cmd=="delids"
+when "delids"
 	req =mk_request(TRP::Message::Command::STAB_PUBSUB_CTL,
 					 :ctl => TRP::SubscribeCtl::CtlType::CT_UNSUBSCRIBE,
 					 :type => TRP::SubscribeCtl::StabberType::ST_ALERT,
 					 :guid => TrisulRP::Guids::AG_IDS)
-elsif cmd=="addtop"
+when "addtop"
 	req =mk_request(TRP::Message::Command::STAB_PUBSUB_CTL,
 					 :ctl => TRP::SubscribeCtl::CtlType::CT_SUBSCRIBE,
 					 :type => TRP::SubscribeCtl::StabberType::ST_TOPPER,
 					 :guid => TrisulRP::Guids::CG_HOST,
 					 :meterid  => 0)
-elsif cmd=="deltop"
+when "deltop"
 	req =mk_request(TRP::Message::Command::STAB_PUBSUB_CTL,
 					 :ctl => TRP::SubscribeCtl::CtlType::CT_UNSUBSCRIBE,
 					 :type => TRP::SubscribeCtl::StabberType::ST_TOPPER,
 					 :guid => TrisulRP::Guids::CG_HOST )
-elsif cmd=="addflow"
+when "addflow"
 	req =mk_request(TRP::Message::Command::STAB_PUBSUB_CTL,
 					 :ctl => TRP::SubscribeCtl::CtlType::CT_SUBSCRIBE,
 					 :type => TRP::SubscribeCtl::StabberType::ST_FLOW,
 					 :guid => TrisulRP::Guids::SG_TCP,
 					 :key  => 'p-0050' )
+when  "addcgraw"
+	raise "addcgraw needs guid key" unless ARGV.length == 2
+	mk_request(TRP::Message::Command::STAB_PUBSUB_CTL,
+					 :ctl => TRP::SubscribeCtl::CtlType::CT_SUBSCRIBE,
+					 :type => TRP::SubscribeCtl::StabberType::ST_COUNTER_ITEM,
+					 :guid => ARGV.shift,
+					 :key => ARGV.shift)
+when "delcg"
+	raise "delcgraw needs guid key" unless ARGV.length == 2
+
+	req =mk_request(TRP::Message::Command::STAB_PUBSUB_CTL,
+					 :ctl => TRP::SubscribeCtl::CtlType::CT_UNSUBSCRIBE,
+					 :type => TRP::SubscribeCtl::StabberType::ST_COUNTER_ITEM,
+					 :guid => ARGV.shift,
+					 :key => ARGV.shift)
 end
 
 s1.send_string(req.serialize_to_string)
