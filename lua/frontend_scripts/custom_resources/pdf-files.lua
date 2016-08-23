11d22f0b8400ec7d290cb7e312847117d932f17e
vivekrajan <vivek@unleashnetworks.com>
2016-08-23 18:11:12 +0530

    Custom alert group sample and custom resource group sample

---
+++ lua/frontend_scripts/custom_resources/pdf-files.lua
@@ -0,0 +1,28 @@
+-- .lua
+--
+-- pdf-files Special Custom Resource 
+--
+--
+--
+TrisulPlugin = {
+
+  id = {
+    name = "PDF Files",
+    description = "PDF File Resources ",
+    author = "Unleash",
+    version_major = 1,
+    version_minor = 0,
+  },
+
+  alertgroup  = {
+    control = {
+      guid = "{79237742-edeb-421e-bd92-b5c62e7f2346}", 
+      name = "PDF File Resource",
+      description = "PDF File Custom Resource ",
+    },
+
+  },
+
+
+}
+
