d99dedf074ad2c42381842724bbb4d6b03c2170b
vivekrajan <vivek@unleashnetworks.com>
2016-09-06 16:44:33 +0530

    filehash.lua demonstrates how to access new resources as they stream by..

--- lua/backend_scripts/resources/filehash.lua
+++ lua/backend_scripts/resources/filehash.lua
@@ -19,9 +19,15 @@ TrisulPlugin = {
 
 	resource_monitor   = {
 
+		-- 
+		-- The guid {978.. below represents the File Hashes resource
+		-- Login as admin/admin , then context0 > Resource Groups to view list of 
+		-- installed resource GUIDs
+		--
 	    resource_guid = '{9781db2c-f78a-4f7f-a7e8-2b1a9a7be71a}',
 
 		onnewresource  = function(engine, newresource )
+			print("timestamp "..  os.date('%c',newresource:timestamp()))
 			print("hash = ".. newresource:uri())
 			print("label  = ".. newresource:label())
 			print("flow  = ".. newresource:flow():to_s())
