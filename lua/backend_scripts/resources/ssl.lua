18245b52f13264c0765eb7479f7ebf48d8495673
vivekrajan <vivek@unleashnetworks.com>
2016-10-17 16:44:27 +0530

    New sample : ssl.lua is a resource monitor, watches SSL/TSL Certs and then prints and counts unique ones

---
+++ lua/backend_scripts/resources/ssl.lua
@@ -0,0 +1,36 @@
+-- ssl.lua
+--
+-- Sample skeleton script for monitoring resources 
+
+--
+-- 
+
+
+TrisulPlugin = {
+
+	id =  {
+		name = "Prints SSL Cert resources ",
+		description = "Just prints SSL Certs ",
+	},
+
+
+	resource_monitor   = {
+
+		-- want the SSL 
+		resource_guid = function()
+			for name ,guid in pairs(T.resourcegroups) do 
+				if name:match("SSL") then return guid; end
+			end
+		end,
+
+		-- a new resource  was seen - print all details to screen 
+		onnewresource  = function(engine, newresource )
+			print("---------------------------------------------------------------------------------------------")
+			print("timestamp "..  os.date('%c',newresource:timestamp()))
+			print("certsubject = ".. newresource:uri())
+			print("flow  = ".. newresource:flow():to_s())
+		end,
+
+	},
+
+}
