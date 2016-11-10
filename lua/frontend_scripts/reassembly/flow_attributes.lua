f9ad1509853a6f94977a184256270b76760dd89c
vivekrajan <vivek@unleashnetworks.com>
2016-11-10 18:06:59 +0530

    new script flow-attrib prints HTTP Host values

---
+++ lua/frontend_scripts/reassembly/flow_attributes.lua
@@ -0,0 +1,37 @@
+-- flow-attributes.lus
+-- 
+-- 	Prints HTTP Hosts - a flow attribute discovered by the Trisul Reassembly Pipeline 
+-- 
+--
+TrisulPlugin = {
+
+
+	id =  {
+
+		name = "flowattrib",
+		description = "prints HTTP Host attributes ",
+	},
+
+	onload = function()
+
+
+	end,
+
+
+	reassembly_handler  = {
+
+
+
+		-- Attributes are discovered by Trisul Reassembly, you can 
+		-- see entire list of attributes in the LUA Docs. Here we 
+		-- just print the HTTP-Host values 
+
+		onattribute  = function(engine, time, flow, attr_type, attr_value )
+
+			if attr_type == "HTTP-Host" then
+				print("Attribute "..attr_type.." = "..attr_value)
+			end
+		  end,
+	},
+
+}
