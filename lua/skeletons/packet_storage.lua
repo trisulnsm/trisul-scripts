d1c8505673a308fadb0ae8b852bad4b6b758e874
vivekrajan <vivek@unleashnetworks.com>
2017-07-12 13:57:08 +0530

    packet storage skeleton : allow you to inspect first few bytes of packets before deciding storage

--- lua/skeletons/packet_storage.lua
+++ lua/skeletons/packet_storage.lua
@@ -51,10 +51,21 @@ TrisulPlugin = {
     -- 
     filter = function( engine, timestamp, flow ) 
       -- your lua code
-      -- return a number from -1..6 representing how to handle
-      -- packet storage for this flow. See docs 
+      -- return a number from -1..6 representing how to handle packet storage for this flow.
+      -- See docs for packet_storage for values
       return -1
     end,
+
+
+    -- WHEN CALLED: the first reassembled chunk in each direction is seen
+    -- use this if you need to see the actual first few bytes of the flow 
+    -- before you decide you want to store the flow or not 
+    filter_payload = function(engine, timestamp, flowkey, direction, seekpos, buffer) 
+      -- your lua code
+      -- return a number from -1..6 representing how to handle packet storage for this flow.
+      -- See docs for packet_storage for values
+      return -1
+    end,    
 
   },
 }
