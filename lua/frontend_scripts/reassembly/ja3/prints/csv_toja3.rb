cdbbb47dfe66fbfebc8676b10beb3e5e5a441650
vivekrajan <vivek@unleashnetworks.com>
2018-02-09 18:03:53 +0530

    Reversed hash and print due to error in csv_tojson

--- lua/frontend_scripts/reassembly/ja3/prints/csv_toja3.rb
+++ lua/frontend_scripts/reassembly/ja3/prints/csv_toja3.rb
@@ -12,9 +12,11 @@ CSV_ORDER = {
 p ARGV 
 
 rows = CSV.foreach( ARGV[0], {headers:false,skip_lines:/^#/ }) do |row| 
-	out = { :desc => row[0],
-	        :ja3_hash => row[1].gsub(/"/,''),
-			:ja3_string => ''}
+	out = {
+	        :desc => row[1].gsub(/"/,''),
+			:ja3_hash => row[0],
+			:ja3_string => ''
+		   }
 	puts out.to_json
 
 
