fedf7c886d2062e2325bd4482d2c5e0d81bfde5b
vivekrajan <vivek@unleashnetworks.com>
2018-02-09 18:06:37 +0530

    CSV to JA3 ruby script fix

--- lua/frontend_scripts/reassembly/ja3/prints/csv_toja3.rb
+++ lua/frontend_scripts/reassembly/ja3/prints/csv_toja3.rb
@@ -3,22 +3,18 @@ require 'csv'
 
 raise "Usage : csv_toja3 <csv_fingerprint file >" unless ARGV.length==1
 
-
 CSV_ORDER = {
-	ja3_hash:0,
-	description:1
+  ja3_hash:0,
+  description:1
 }
 
-p ARGV 
 
 rows = CSV.foreach( ARGV[0], {headers:false,skip_lines:/^#/ }) do |row| 
-	out = {
-	        :desc => row[1].gsub(/"/,''),
-			:ja3_hash => row[0],
-			:ja3_string => ''
-		   }
-	puts out.to_json
-
-
+  out = {
+          :desc => row[1].gsub(/"/,''),
+          :ja3_hash => row[0],
+          :ja3_string => ''
+        }
+  puts out.to_json
 end
 
