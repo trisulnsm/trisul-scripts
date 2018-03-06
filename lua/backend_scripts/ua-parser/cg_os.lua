7edcf1aee425fd8161352e06f5ef625006310da4
vivekrajan <vivek@unleashnetworks.com>
2018-03-06 20:39:47 +0530

    User Agent new samples

---
+++ lua/backend_scripts/ua-parser/cg_os.lua
@@ -0,0 +1,31 @@
+--
+-- UA-OS counter group 
+-- 
+-- 
+TrisulPlugin = { 
+
+  -- the ID block, you can skip the fields marked 'optional '
+  -- 
+  id =  {
+    name = "UA OS",
+    description = "Browser hits ", -- optional
+  },
+
+
+  -- countergroup block
+  -- 
+  countergroup = {
+
+    control = {
+      guid = "{0F67F47E-A407-4047-2AF6-8E25FEC75C3A}",
+      name = "UA OS",
+      description = "OS hits based on UA",
+      bucketsize = 30,
+    },
+
+    meters = {
+        {  0, T.K.vartype.COUNTER,      10, 0, "Hits",  "hits",  "hits" },
+    },  
+
+  },
+}
