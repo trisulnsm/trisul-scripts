d5a3a2b99058674faceccaaa9257e174ef023ea0
vivekrajan <vivek@unleashnetworks.com>
2018-09-11 12:55:33 +0530

    protocol tree maker

---
+++ lua/frontend_scripts/protocol_tree/README.md
@@ -0,0 +1,6 @@
+Protocol Tree Maker
+===================
+
+Pop this script into Trisul to create a protocol tree of your network traffic. 
+
+
--- lua/frontend_scripts/protocol_tree/ptree_ethernet.lua
+++ lua/frontend_scripts/protocol_tree/ptree_ethernet.lua
@@ -29,21 +29,43 @@ TrisulPlugin = {
 	  if etype==0x0800 then
 
 	  	local ihl = sb:next_bitfield_u8( {4,4} ) 
-	  	sb:skip(8)
+	  	sb:skip(6)
+		local flags_fragoff = sb:next_bitfield_u8( {1,1,1,1,12})
+		sb:skip(1)
 		local proto=sb:next_u8()
 		sb:skip(4*ihl[2] - 10 ) 
 		table.insert(protostack,proto)
 
-		if proto == 6 or proto == 11 then 
+		if flags_fragoff[3]==1 or flags_fragoff[5] > 0 then
+			table.insert(protostack,"frag")
+		elseif proto == 6 or proto == 17 then 
 			local useport  = math.min(sb:next_u16(), sb:next_u16())
 			if useport < 1024 then
 				table.insert(protostack,useport)
 			else
 				table.insert(protostack,65535)
 			end
+		elseif proto==1 then
+			table.insert(protostack,sb:next_u8())
 		end 
 
-	  elseif etype==0x0884 then
+	  elseif etype==0x86DD then
+	  	sb:skip(6)
+		local proto=sb:next_u8()
+		table.insert(protostack,proto)
+
+		sb:skip(33)
+		if proto == 6 or proto == 17 then 
+			local useport  = math.min(sb:next_u16(), sb:next_u16())
+			if useport < 1024 then
+				table.insert(protostack,useport)
+			else
+				table.insert(protostack,65535)
+			end
+		elseif proto==1 then
+			table.insert(protostack,sb:next_u8())
+		end 
+
 
 	  else 
 
@@ -51,7 +73,11 @@ TrisulPlugin = {
 
 	  local key = table.concat(protostack,"/")
 
-	  print(key)
+	  -- print(key)
+
+	  engine:update_counter_bytes("{2CF4DCFF-77E5-45E9-AA03-8D827CE0813C}",key,0)
+	  engine:update_counter("{2CF4DCFF-77E5-45E9-AA03-8D827CE0813C}",key,1,1)
+
 
     end,
 
