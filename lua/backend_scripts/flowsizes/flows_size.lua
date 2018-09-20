a7ade1d01203c732819c5f48e4c77db03541e191
vivekrajan <vivek@unleashnetworks.com>
2018-09-20 19:33:02 +0530

    flow sizes

---
+++ lua/backend_scripts/flowsizes/flows_size.lua
@@ -0,0 +1,43 @@
+--
+-- flows sizes 
+-- 
+TrisulPlugin = { 
+
+  id =  {
+    name = "Flowsizes",
+    description = "flow size analytics", -- optional
+  },
+
+  sg_monitor  = {
+
+    onflush = function(engine, flow) 
+
+		-- packet size into  log10 range  
+		local totalbytes = flow:az_bytes()  + flow:za_bytes() 
+		local key = "".. math.ceil( math.log10(totalbytes))
+
+		engine:update_counter( "{91D08D08-B846-4C28-1FC9-A2C419DCC605}", key, 0, 1)
+		engine:update_counter( "{91D08D08-B846-4C28-1FC9-A2C419DCC605}", key, 1, totalbytes)
+    end,
+
+  }
+
+  countergroup = {
+
+    control = {
+      guid = "{91D08D08-B846-4C28-1FC9-A2C419DCC605}",
+      name = "Flow Sizes",
+      description = "Flow sizes distributio",
+      bucketsize = 60,
+    },
+
+    -- meters table
+    meters = {
+        {  0, T.K.vartype.COUNTER,      50, 30, "Flows",  "flows",  "Flws" },
+        {  1, T.K.vartype.COUNTER,      50, 30, "Bytes",  "bytes",  "Bytes" },
+    },  
+
+  },
+
+
+}
