ce7fb67fa20f1b75186e1bac002ca9730ff39e9e
vivekrajan <vivek@unleashnetworks.com>
2018-09-21 13:27:33 +0530

    flow size

--- lua/backend_scripts/flowsizes/README.md
+++ lua/backend_scripts/flowsizes/README.md
@@ -1,5 +1,5 @@
 Flow Sizes Analytics
-===================
+====================
 
 This script measures the relationship between flow sizes and the data transferred. 
 
--- lua/backend_scripts/flowsizes/flows_size.lua
+++ lua/backend_scripts/flowsizes/flows_size.lua
@@ -1,5 +1,4 @@
---
--- flows sizes 
+-- flows sizes vs volume distribution 
 -- 
 TrisulPlugin = { 
 
@@ -8,19 +7,21 @@ TrisulPlugin = {
     description = "flow size analytics", -- optional
   },
 
+
   sg_monitor  = {
 
     onflush = function(engine, flow) 
 
-		-- packet size into  log10 range  
-		local totalbytes = flow:az_bytes()  + flow:za_bytes() 
-		local key = "".. math.ceil( math.log10(totalbytes))
+      -- packet size into  log10 range  
+      local totalbytes = flow:az_bytes()  + flow:za_bytes() 
+      local key = "".. math.ceil( math.log10(totalbytes))
 
-		engine:update_counter( "{91D08D08-B846-4C28-1FC9-A2C419DCC605}", key, 0, 1)
-		engine:update_counter( "{91D08D08-B846-4C28-1FC9-A2C419DCC605}", key, 1, totalbytes)
+      engine:update_counter( "{91D08D08-B846-4C28-1FC9-A2C419DCC605}", key, 0, 1)
+      engine:update_counter( "{91D08D08-B846-4C28-1FC9-A2C419DCC605}", key, 1, totalbytes)
     end,
+  },
 
-  }
+
 
   countergroup = {
 
@@ -33,10 +34,9 @@ TrisulPlugin = {
 
     -- meters table
     meters = {
-        {  0, T.K.vartype.COUNTER,      50, 30, "Flows",  "flows",  "Flws" },
-        {  1, T.K.vartype.COUNTER,      50, 30, "Bytes",  "bytes",  "Bytes" },
+      {  0, T.K.vartype.COUNTER,      50, 30, "Flows",  "flows",  "Flws" },
+      {  1, T.K.vartype.COUNTER,      50, 30, "Bytes",  "bytes",  "Bytes" },
     },  
-
   },
 
 
