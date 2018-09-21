5deb2590fad8b2d70cc2c3dc8baac7148270628b
vivekrajan <vivek@unleashnetworks.com>
2018-09-21 18:19:09 +0530

    flows distribution

--- lua/backend_scripts/flowsizes/flows_size.lua
+++ lua/backend_scripts/flowsizes/flows_size.lua
@@ -11,16 +11,15 @@ TrisulPlugin = {
   sg_monitor  = {
 
     onflush = function(engine, flow) 
-
       -- packet size into  log10 range  
       local totalbytes = flow:az_bytes()  + flow:za_bytes() 
-      local key = "".. math.ceil( math.log10(totalbytes))
-
-      engine:update_counter( "{91D08D08-B846-4C28-1FC9-A2C419DCC605}", key, 0, 1)
-      engine:update_counter( "{91D08D08-B846-4C28-1FC9-A2C419DCC605}", key, 1, totalbytes)
+      if totalbytes > 0 then 
+        local key =  tostring(math.ceil( math.log10(totalbytes))) .. " Dig"
+        engine:update_counter( "{91D08D08-B846-4C28-1FC9-A2C419DCC605}", key, 0, 1)
+        engine:update_counter( "{91D08D08-B846-4C28-1FC9-A2C419DCC605}", key, 1, totalbytes)
+      end 
     end,
   },
-
 
 
   countergroup = {
@@ -28,7 +27,7 @@ TrisulPlugin = {
     control = {
       guid = "{91D08D08-B846-4C28-1FC9-A2C419DCC605}",
       name = "Flow Sizes",
-      description = "Flow sizes distributio",
+      description = "Flow sizes distribution",
       bucketsize = 60,
     },
 
@@ -38,6 +37,5 @@ TrisulPlugin = {
       {  1, T.K.vartype.COUNTER,      50, 30, "Bytes",  "bytes",  "Bytes" },
     },  
   },
-
-
 }
+
