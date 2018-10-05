ec8f23399ca152dbf75ef599efb5333a4487689b
vivekrajan <vivek@unleashnetworks.com>
2018-10-05 17:03:56 +0530

    rbench

--- lua/backend_scripts/unixsocket_api/rubyexample/rbench.rb
+++ lua/backend_scripts/unixsocket_api/rubyexample/rbench.rb
@@ -3,9 +3,12 @@ require 'net/https'
 
 
 # set up unix socket
-apis = UNIXSocket.new("/usr/local/var/lib/trisul-probe/domain0/probe0/context0/run/api.sock.0")
+apis = Socket.new(:UNIX, :DGRAM)  # UNIX datagram socket
+s1_ai = Socket.sockaddr_un("/usr/local/var/lib/trisul-probe/domain0/probe0/context0/run/api.sock.0")
+apis.connect(s1_ai)
 
-uri = URI.parse("https://192.168.2.99")
+#uri = URI.parse("https://192.168.2.99")
+uri = URI.parse("https://api.saas.paytm.com")
 request = Net::HTTP.new(uri.host, uri.port)
 
 while true do 
@@ -15,13 +18,15 @@ while true do
 
   us_latency  = t2-t1
 
-  apis.send("update_counter\n{9497A90C-86DF-44A5-439F-3B4092792728}\ntest_192.168.2.99=\n0\n1")
+  apis.send("update_counter\n{9497A90C-86DF-44A5-439F-3B4092792728}\ntest_192.168.2.99=\n0\n1",0)
 
-  apis.send("update_counter\n{9497A90C-86DF-44A5-439F-3B4092792728}\ntest_192.168.2.99=\n1\n#{us_latency}")
+  apis.send("update_counter\n{9497A90C-86DF-44A5-439F-3B4092792728}\ntest_192.168.2.99=\n1\n#{us_latency}",0)
 
-  apis.send("update_counter\n{9497A90C-86DF-44A5-439F-3B4092792728}\ntest_192.168.2.99=\n2\n#{us_latency}")
+  apis.send("update_counter\n{9497A90C-86DF-44A5-439F-3B4092792728}\ntest_192.168.2.99=\n2\n#{us_latency}",0)
 
-  apis.send("update_counter\n{9497A90C-86DF-44A5-439F-3B4092792728}\ntest_192.168.2.99=\n3\n#{us_latency}")
+  apis.send("update_counter\n{9497A90C-86DF-44A5-439F-3B4092792728}\ntest_192.168.2.99=\n3\n#{us_latency}",0)
+
+  puts(us_latency)
 
 end
 
