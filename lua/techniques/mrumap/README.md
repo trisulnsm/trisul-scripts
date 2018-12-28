18bd5218e8f08ed81a95abe94c174af257ad27ec
vivekrajan <vivek@unleashnetworks.com>
2018-12-28 18:32:10 +0530

    mrumap

---
+++ lua/techniques/mrumap/README.md
@@ -0,0 +1,11 @@
+# mrumap : pure lua lookup table with O(1) LRU prune 
+
+A map backed by a MRU list that allows you to prune the least recently used keys in O(1) 
+
+
+## Use case
+
+* Lookup table to hold state per-flow.  
+
+
+
---
+++ lua/techniques/mrumap/test.lua
@@ -0,0 +1,47 @@
+local mrumap=require'mrumap'
+
+local tst  = mrumap.new()
+print(tst:get("hi"))
+
+tst:put('oldest',1)
+tst:put('secondoldest',1)
+tst:put('oldestbutaccessedlater',1)
+
+-- 
+local function randstr() 
+local chars={}
+for i=1,10 do 
+  table.insert(chars,string.char(math.random(97, 122)))
+end
+return table.concat(chars,"")
+end 
+
+-- insert about 
+for i = 1, 25 do 
+tst:put(randstr(),1)
+end 
+
+tst:get("oldestbutaccessedlater")
+
+-- print in MRU order 
+for k in tst:mru_iter()  do
+print(k)
+end
+print("size="..tst:size())
+
+-- pop the LRU item
+tst:pop_back()
+
+for k in tst:mru_iter()  do
+print(k)
+end
+
+print("size="..tst:size())
+
+-- roll back all to zero
+while tst:pop_back() do
+print("size="..tst:size())
+end
+
+print("size="..tst:size())
+
