8fa36f103df328d87708b61a22e74699924b75d5
vivekrajan <vivek@unleashnetworks.com>
2018-12-29 18:07:47 +0530

    ICMP echo -check tunnel detector

---
+++ lua/frontend_scripts/pingtunnel/README.md
@@ -0,0 +1,5 @@
+# ICMP Mega Tunnel Detector 
+
+echo reply style  
+pTunnel style
+
--- lua/frontend_scripts/pingtunnel/icmp-echo-check.lua
+++ lua/frontend_scripts/pingtunnel/icmp-echo-check.lua
@@ -18,9 +18,9 @@ TrisulPlugin = {
     description = "Payload in ECHO/REPLY must match in real PINGs", 
   },
 
-
   onload = function()
-	 T.icmp_echo_pending =  MM.new() 
+    T.icmp_echo_pending =  MM.new() 
+    T.unsolicited =  MM.new() 
   end,
 
   -- simple_counter  block
@@ -30,54 +30,67 @@ TrisulPlugin = {
     -- latch on to ICMP protocol 
     protocol_guid = "{7DDD65F2-A316-43B5-A103-368E700E045E}",
 
-
     -- icmp lands here 
     onpacket = function(engine,layer)
-	    local sb = SB.new( layer:rawbytes():tostring() )
-	    local t =sb:next_u8()
-	    if t ~= 0 and t ~= 8 then return end
+      local sb = SB.new( layer:rawbytes():tostring() )
+      local t =sb:next_u8()
+      if t ~= 0 and t ~= 8 then return end
 
-	    local c,_,id,seqno=  sb:next_u8(),
-				 sb:next_u16(),
-				 sb:next_u16(),
-				 sb:next_u16()
+      local c,_,id,seqno=  sb:next_u8(),
+                           sb:next_u16(),
+                           sb:next_u16(),
+                           sb:next_u16()
 
-	    local paystr = sb:buffer_left()
-	    local hsh=T.util.hash(paystr,32)
-	    local flowid=layer:packet():num_layers()
+      local paystr = sb:buffer_left()
+      local hsh=T.util.hash(paystr,32)
+      local flowid=layer:packet():num_layers()
 
-	    -- need the ips + id 
-	    local icmpflowkey=layer:packet():flowid():id() 
-		print(icmpflowkey)
+      -- need the ips + id 
+      local icmpflowkey=layer:flowkey().."-"..id.."-"..seqno
 
-	    if t==0 then
-		    T.icmp_echo_pending:put(icmpflowkey,hsh)
-		    print("\nadding    "..icmpflowkey.." = "..hsh )
-	    else
-		    -- check replies hash
-		    --
-		    local req_hash = T.icmp_echo_pending:get(icmpflowkey)
-		    print("checking  -"..icmpflowkey.." = "..req_hash)
-		    if req_hash and req_hash ~= hsh then
-			    print("Alert tunnel on ? "..icmpflowkey)
-			    -- HEY! funny stuff, req and resp payloads dont match
-			    -- something gooffy, raise alert
-			    engine:add_alert("{B5F1DECB-51D5-4395-B71B-6FA730B772D9}",
-			    		     "06A:00.00.00.00_icmp00:00.00.00.00_icmp01",
-					     "PING-TUNNEL",
-					     1,
-					     "Possible ICMP Echo (Ping) tunnel, payloads dont match")
-		    end
-		    T.icmp_echo_pending:delete(icmpflowkey)
-	    end 
+      if t==8 then
+        --print("INS: "..icmpflowkey.. "hsh="..hsh.. " sz="..T.icmp_echo_pending:size())
+        T.icmp_echo_pending:put(icmpflowkey,hsh)
+      else
+        -- check replies hash
+        --
+        local resp_hash = T.icmp_echo_pending:get(icmpflowkey)
+        if resp_hash and resp_hash ~= hsh then
+          print("Alert tunnel on ? "..icmpflowkey)
 
-		-- cap at 100
-		if T.icmp_echo_pending:size() > 100 then 
-			T.icmp_echo_pending:pop_back()
-		end
+          -- hey ! funny stuff, req and resp payloads dont match raise an alert, 
+          engine:add_alert("{B5F1DECB-51D5-4395-B71B-6FA730B772D9}",
+                    "06A:00.00.00.00_icmp00:00.00.00.00_icmp01",
+                    "PING-TUNNEL",
+                    1,
+                    "Possible ICMP Echo (Ping) tunnel, payloads dont match")
+        elseif not resp_hash then 
+          local u=T.unsolicited:get(layer:flowkey())  or 0 
+          T.unsolicited:put(layer:flowkey(),u+1)
+          if u== 5 then
+            print("Hey, looks like a ptunnel type hole puncher? "..icmpflowkey)
+            
+            -- hey looks like pTunnel ! funny stuff, too many unsolicited
+            engine:add_alert("{B5F1DECB-51D5-4395-B71B-6FA730B772D9}",
+                     "06A:00.00.00.00_icmp00:00.00.00.00_icmp01",
+                     "PING-TUNNEL",
+                     1,
+                     "Possible ICMP pTunnel style, unsolicited replies")
+          end
+        end
+        T.icmp_echo_pending:delete(icmpflowkey)
+      end 
 
+      -- cap at 100
+      if T.icmp_echo_pending:size() > 100 then 
+        T.icmp_echo_pending:pop_back()
+      end
+
+      -- unsolicited cap at 100 
+      if T.unsolicited:size() > 100 then 
+        T.unsolicited:pop_back()
+      end
     end,
-
 
   },
 }
