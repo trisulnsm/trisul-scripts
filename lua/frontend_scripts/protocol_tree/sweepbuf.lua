f2997458909d0a80ce41e5b35c7d4bf123fc8f5e
vivekrajan <vivek@unleashnetworks.com>
2019-03-13 13:21:59 +0530

    updated sweepbuf

--- lua/frontend_scripts/protocol_tree/sweepbuf.lua
+++ lua/frontend_scripts/protocol_tree/sweepbuf.lua
@@ -98,6 +98,8 @@ local SweepBuf  = {
 		r=tbl:next_u24()
 	elseif nbytes==4 then
 		r=tbl:next_u32()
+	elseif nbytes==0 then
+		return nil
 	else
 		error("next_uN : only supports 1,2,3,4 byte numbers. Given="..nbytes)
 	end
@@ -187,6 +189,17 @@ local SweepBuf  = {
     end 
   end,
 
+  next_hex_str_to_len = function(tbl, slen)
+  	local ret = tbl:next_str_to_len(slen)
+	if ret then
+	    return (ret:gsub('.', function (c)
+		        return string.format('%02X', string.byte(c))
+		end))
+	else
+		return nil
+	end
+  end,
+
   next_u16_arr = function(tbl,nitems)
     local ret = {}
     while nitems > 0 do
@@ -242,6 +255,10 @@ local SweepBuf  = {
   has_more = function(tbl)
     return tbl.seekpos < tbl:top_fence() 
   end,
+
+  buffer_left = function(tbl)
+    return string.sub(tbl.buff ,tbl.seekpos+1, #tbl.buff)
+  end, 
 
   bytes_left = function(tbl)
     return #tbl.buff - tbl.seekpos + 1
@@ -306,6 +323,31 @@ local SweepBuf  = {
 	end
 	return ret
   end,
+
+
+  -- 8-bit fields with fieldnames
+  next_bitfield_u8_named  = function(tbl, bitmap, fieldnames )
+  	local values=tbl:next_bitfield_u8(bitmap)
+	local ret={}
+
+	for i = 1 , #values do 
+		ret[fieldnames[i]]=values[i]
+	end 
+
+	return ret
+  end, 
+
+
+  -- 16-bits with fieldnames 
+  next_bitfield_u16_named  = function(tbl, bitmap, fieldnames )
+  	local values=tbl:next_bitfield_u16(bitmap)
+	local ret={}
+	for i = 1 , #values do 
+		ret[fieldnames[i]]=values[i]
+	end 
+	return ret
+  end, 
+
 
   next_ipv4 = function(tbl)
   	return string.format("%d.%d.%d.%d", tbl:next_u8(), tbl:next_u8(), tbl:next_u8(), tbl:next_u8())
