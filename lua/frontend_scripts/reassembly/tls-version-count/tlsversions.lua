230f62a42299e8a9ec24407959c760600b9665ef
vivek <vivek@fedo30.localdomain>
2020-01-20 19:18:51 +0530

    TLS edges

--- lua/frontend_scripts/reassembly/tls-version-count/tlsversions.lua
+++ lua/frontend_scripts/reassembly/tls-version-count/tlsversions.lua
@@ -18,7 +18,7 @@ TrisulPlugin = {
     filter = function(engine, timestamp, flow )
 
       return  flow:porta_readable() == "443" or 
-	          flow:portz_readable() == "443"  
+              flow:portz_readable() == "443"  
 
     end,
 
@@ -27,70 +27,75 @@ TrisulPlugin = {
     onattribute = function(engine, timestamp, flowkey, attr_name, attr_value) 
 
 
-	    if attr_name ~= "TLS:RECORD" then return;  end 
-	    
-	    local payload = SWP.new(attr_value)
+      if attr_name ~= "TLS:RECORD" then return;  end 
+      
+      local payload = SWP.new(attr_value)
 
-	    -- Only interested in TLS handshake (type = 22) + client_hello only
-	    if payload:next_u8() == 22 and payload:skip(4) and payload:next_u8()==2 then
+      -- Only interested in TLS handshake (type = 22) + client_hello only
+      if payload:next_u8() == 22 and payload:skip(4) and payload:next_u8()==2 then
 
+        payload:reset()
+        payload:inc(5)
+        payload:next_u8()                    -- over handshake_type
 
-	        payload:reset()
+        -- validate , sometimes encrypted handshake can misfire 
+        local hslen=payload:next_u24()                  
+        if hslen ~= #attr_value - 9 then return end;
 
-	        payload:inc(5)
-	        payload:next_u8()                   -- over handshake_type
-
-	        -- validate , sometimes encrypted handshake can misfire 
-	        local hslen=payload:next_u24()                  
-	        if hslen ~= #attr_value - 9 then return end;
-	        local hs_version=payload:next_u16()                   -- over version 
-
-
-
-	        payload:skip(32)                    -- over client_random
-	        payload:skip(payload:next_u8())     -- over SessionID if present 
-	        payload:next_u16() 					-- cipher 
-			payload:skip(1)                     -- over compression 
+        local hs_version=payload:next_u16() -- over TLS version 
+        payload:skip(32)                    -- over client_random
+        payload:skip(payload:next_u8())     -- over SessionID if present 
+        payload:next_u16()                  -- cipher 
+        payload:skip(1)                     -- over compression 
 
 
-	        if not payload:has_more() then return end
+        if not payload:has_more() then return end
 
 
-			-- extensions length
-	        payload:push_fence(payload:next_u16())
-
-			print(payload:hexdump_left() ) 
+        -- extensions length
+        payload:push_fence(payload:next_u16())
 
 
-			local version = nil 
-	        while payload:has_more() do
-				local ext_type = payload:next_u16()
-				local ext_len =  payload:next_u16()
-				if ext_type == 43 then
-					version = payload:next_uN(ext_len) 
-				else
-					payload:skip(ext_len)
-				end
+        -- search for supported_version(43) extension 
+        local version = nil 
+        while payload:has_more() do
+          local ext_type = payload:next_u16()
+          local ext_len =  payload:next_u16()
+          if ext_type == 43 then
+            version = payload:next_uN(ext_len) 
+          else
+            payload:skip(ext_len)
+          end
+        end
 
-	        end
-	        if version == nil then
-	        	version = hs_version
-	        end
+        -- if no version in server hello extension, then use the TLS version in SH
+        if version == nil then
+          version = hs_version
+        end
 
-	        -- meter the version here 
-	        engine:update_counter("{44DBB4E0-B79F-4FB5-A437-3CFEDFB7B65E}",
-	        					  tostring(version),
-	        					  1,
-	        					  1)
+        -- meter the version here 
+        engine:update_counter("{44DBB4E0-B79F-4FB5-A437-3CFEDFB7B65E}",
+                    tostring(version),
+                    1,
+                    1)
 
-	        engine:add_flow_counter(flowkey:id(),
-	        					  "{44DBB4E0-B79F-4FB5-A437-3CFEDFB7B65E}",
-	        					  tostring(version),
-	        					  0,
-	        					  0)
-	    end
-	end,    
+        engine:add_flow_counter(flowkey:id(),
+                    "{44DBB4E0-B79F-4FB5-A437-3CFEDFB7B65E}",
+                    tostring(version),
+                    0,
+                    0)
 
+        -- edges to flows
+        engine:add_flow_edges(flowkey:id(),
+                    "{44DBB4E0-B79F-4FB5-A437-3CFEDFB7B65E}",
+                    tostring(version))
+
+        -- we're done with this flow, 
+        -- if no other plugin in interested then Trisul will stop reassembling 
+        engine:disable_reassembly(flowkey:id())
+
+      end
+    end,    
   },
 
 -- countergroup block
@@ -98,8 +103,8 @@ TrisulPlugin = {
   countergroup = {
 
     -- control table 
-	-- specify details of your new counter group you can use 
-	-- 'trisulctl_probe testbench guid' to get a new GUID
+  -- specify details of your new counter group you can use 
+  -- 'trisulctl_probe testbench guid' to get a new GUID
     control = {
       guid = "{44DBB4E0-B79F-4FB5-A437-3CFEDFB7B65E}",
       name = "TLS Versions",
@@ -116,6 +121,7 @@ TrisulPlugin = {
     -- key mapping
     -- maps keys used by you into user friendly names for Trisul display 
     keyinfo = {
+      {"768","SSL 3.0"},
       {"769","TLS 1.0"},
       {"770","TLS 1.1"},
       {"771","TLS 1.2"},
