3f45d8bfd9bdff4a01587668229427d6fdc5a26e
Vivek Rajagopalan <vivek@unleashnetworks.com>
2021-04-15 18:55:05 +0530

    Added attachement options for protocol_handler

--- lua/skeletons/protocol_handler.lua
+++ lua/skeletons/protocol_handler.lua
@@ -25,6 +25,10 @@ TrisulPlugin = {
   control = {
     guid  = "{0CED6B98-0D90-475C-D2D7-06A8E9E64B7C}",  -- new protocol GUID, use tp testbench guid to create
     name  = "FLOWDIRECT",  -- new protocol name 
+
+    -- these two are optional 
+    host_protocol_guid = '{14D7AB53-CC51-47e9-8814-9C06AAE60189}', -- GUID for UDP  
+    host_protocol_ports = { 67,68 }                                -- we want UDP ports 67,68
   },
 
 
