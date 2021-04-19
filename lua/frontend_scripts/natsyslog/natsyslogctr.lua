735021184239ffa19ceca15037bce3976e7bd59d
Vivek Rajagopalan <vivek@unleashnetworks.com>
2021-04-19 17:44:48 +0530

    added cisco NAT

--- lua/frontend_scripts/natsyslog/natsyslogctr.lua
+++ lua/frontend_scripts/natsyslog/natsyslogctr.lua
@@ -24,7 +24,10 @@ TrisulPlugin = {
   -- 
   -- WHEN CALLED : your LUA script is loaded into Trisul 
   onload = function()
+
 	T.re2_JioDeviceNATSyslog=T.re2("<141>(\\w+)\\s+(\\d+)\\s+(\\d\\d):(\\d\\d):(\\d\\d)\\s+(\\S+)\\s+NAT_ACCT:(\\w+)\\s+(\\w+)\\s+SourceIp\\s+:(\\S+)\\s+Sourceport:(\\d+)\\s+TransIP\\s+:(\\S+)\\s+TransPort:(\\d+)\\s+DestIp\\s+:(\\S+)\\s+Destport:(\\d+)\\s*")
+
+	T.re2_CiscoNATSyslog=T.re2("(\\w+)\\s+(\\d+)\\s+(\\d\\d):(\\d\\d):(\\d\\d).\\d\\d\\d.*(Created|Deleted)\\s+Translation\\s+(\\w+)\\s+(\\S+):(\\S+)\\s+(\\S+):(\\S+)\\s+(\\S+):(\\S+)\\s+(\\S+):(\\S+)\\d+")
 
   end,
 
@@ -41,33 +44,55 @@ TrisulPlugin = {
     onpacket = function(engine,layer)
 		local syslogstr = layer:rawbytes():tostring()
 
-		local bret, mon, day, h,m,s, deviceip, cmd, proto, sip, sport, tsip, tsport, dip, dport     = T.re2_JioDeviceNATSyslog:full_match_n(syslogstr)
+		if syslogstr:find("NAT_ACCT",1,true) then 
+			-- JIO device 
 
-		if bret ==false then
-			return
-		end
+			local bret, mon, day, h,m,s, deviceip, cmd, proto, sip, sport, tsip, tsport, dip, dport = T.re2_JioDeviceNATSyslog:partial_match_n(syslogstr)
 
-		-- print( mon.." "..day.." "..h.." "..deviceip..' '..cmd..' '..proto..' '..sip..' '..sport..' '..tsip..' '..tsport..' '..dip..' '..dport) 
+			if bret ==false then return;  end
 
-		local tvsec = os.time( {
-			year = tonumber(os.date('%Y')),
-			month = MONTHNAMES[mon],
-			day = tonumber(day),
-			hour =h, min = m, sec = s
-		})
-		local fkey = Fk.toflow_format_v4( proto, sip,sport, dip, dport)
+			local tvsec = os.time( {
+				year = tonumber(os.date('%Y')),
+				month = MONTHNAMES[mon],
+				day = tonumber(day),
+				hour =h, min = m, sec = s
+			})
+			local fkey = Fk.toflow_format_v4( proto, sip,sport, dip, dport)
 
-		if cmd == "START" then
-		print(fkey) 
-			engine:update_flow_raw( fkey, 0, 1)
-			engine:tag_flow ( fkey, "[natip]"..tsip)
-			engine:tag_flow ( fkey, "[natport]"..tsport)
-			engine:tag_flow ( fkey, "[deviceip]"..deviceip)
-		elseif cmd == "STOP" then 
-			engine:terminate_flow ( fkey)
+			if cmd == "START" then
+				engine:update_flow_raw( fkey, 0, 1)
+				engine:tag_flow ( fkey, "[natip]"..tsip)
+				engine:tag_flow ( fkey, "[natport]"..tsport)
+				engine:tag_flow ( fkey, "[deviceip]"..deviceip)
+			elseif cmd == "STOP" then 
+				engine:terminate_flow ( fkey)
+			end 
+
+		elseif syslogstr:find("LOG_TRANSLATION",1,true) then 
+
+			-- CISCO device 
+			local bret, mon, day, h,m,s, cmd, proto, sip, sport, tsip, tsport, dip, dport = T.re2_CiscoNATSyslog:partial_match_n(syslogstr)
+
+			if bret ==false then return; end
+
+			local tvsec = os.time( {
+				year = tonumber(os.date('%Y')),
+				month = MONTHNAMES[mon],
+				day = tonumber(day),
+				hour =h, min = m, sec = s
+			})
+			local fkey = Fk.toflow_format_v4( proto, sip,sport, dip, dport)
+
+			if cmd == "Created" then
+				engine:update_flow_raw( fkey, 0, 1)
+				engine:tag_flow ( fkey, "[natip]"..tsip)
+				engine:tag_flow ( fkey, "[natport]"..tsport)
+			elseif cmd == "Deleted" then 
+				engine:terminate_flow ( fkey)
+			end 
+
 		end 
     end,
-
   },
 }
 
