d714d45b85d62ac0a1adb9f2ab0c91add01dc8dc
vivek <vivek@unleashnetworks.com>
2023-02-14 20:35:12 +0530

    SDEL timestamps for duration compuation

--- lua/frontend_scripts/natsyslog/natsyslogctr.lua
+++ lua/frontend_scripts/natsyslog/natsyslogctr.lua
@@ -4,10 +4,10 @@
 local Fk = require'flowkey' 
 
 MONTHNAMES = {
-	['Jan'] = 1, ['Feb'] = 2, ['Mar'] = 3,
-	['Apr'] = 4, ['May'] = 5, ['Jun'] = 6,
-	['Jul'] = 7, ['Aug'] = 8, ['Sep'] = 9,
-	['Oct'] = 10, ['Nov'] = 11, ['Dec'] = 12,
+  ['Jan'] = 1, ['Feb'] = 2, ['Mar'] = 3,
+  ['Apr'] = 4, ['May'] = 5, ['Jun'] = 6,
+  ['Jul'] = 7, ['Aug'] = 8, ['Sep'] = 9,
+  ['Oct'] = 10, ['Nov'] = 11, ['Dec'] = 12,
 }
 
 TrisulPlugin = { 
@@ -24,15 +24,15 @@ TrisulPlugin = {
   -- 
   -- WHEN CALLED : your LUA script is loaded into Trisul 
   onload = function()
-		T.re2_NetElasticBNGNATSyslog=T.re2('19412\\s(SADD|SDEL)\\s\\[nsess\\sTRIG="(\\w+)"\\sPROTO="(\\d+)"\\sSSUBIX="(\\d)"\\sIATYP="(\\w+)"\\sUSERNAME="(\\w+)"\\sISADDR="(\\S+)"\\sIDADDR="(\\S+)"\\sISPORT="(\\d+)"\\sIDPORT="(\\d+)"\\sXATYP="(\\w+)"\\sXSADDR="(\\S+)"\\sXDADDR="(\\S+)"\\sXSPORT="(\\d+)"\\sXDPORT="(\\d+)"\\]\\stime=\'(\\d{4})-(\\d{2})-(\\d{2}) (\\d{2}):(\\d{2}):(\\d{2})\'')
+    T.re2_NetElasticBNGNATSyslog=T.re2('19412\\s(SADD|SDEL)\\s\\[nsess\\sTRIG="(\\w+)"\\sPROTO="(\\d+)"\\sSSUBIX="(\\d)"\\sIATYP="(\\w+)"\\sUSERNAME="(\\w+)"\\sISADDR="(\\S+)"\\sIDADDR="(\\S+)"\\sISPORT="(\\d+)"\\sIDPORT="(\\d+)"\\sXATYP="(\\w+)"\\sXSADDR="(\\S+)"\\sXDADDR="(\\S+)"\\sXSPORT="(\\d+)"\\sXDPORT="(\\d+)"\\]\\stime=\'(\\d{4})-(\\d{2})-(\\d{2}) (\\d{2}):(\\d{2}):(\\d{2})\'')
 
-		T.re2_HuaweiNATSyslog=T.re2(".*<NAT444>:<(\\w+)>\\s(\\d+)\\|(\\d+|-)\\|(\\S+)\\|(\\S+)\\|(\\d+)\\|(\\S+)\\|(\\d+)\\|(\\d+)")
+    T.re2_HuaweiNATSyslog=T.re2(".*<NAT444>:<(\\w+)>\\s(\\d+)\\|(\\d+|-)\\|(\\S+)\\|(\\S+)\\|(\\d+)\\|(\\S+)\\|(\\d+)\\|(\\d+)")
 
-		T.re2_JioDeviceNATSyslog=T.re2("<141>(\\w+)\\s+(\\d+)\\s+(\\d\\d):(\\d\\d):(\\d\\d)\\s+(\\S+)\\s+NAT_ACCT:(\\w+)\\s+(\\w+)\\s+SourceIp\\s+:(\\S+)\\s+Sourceport:(\\d+)\\s+TransIP\\s+:(\\S+)\\s+TransPort:(\\d+)\\s+DestIp\\s+:(\\S+)\\s+Destport:(\\d+)\\s*")
+    T.re2_JioDeviceNATSyslog=T.re2("<141>(\\w+)\\s+(\\d+)\\s+(\\d\\d):(\\d\\d):(\\d\\d)\\s+(\\S+)\\s+NAT_ACCT:(\\w+)\\s+(\\w+)\\s+SourceIp\\s+:(\\S+)\\s+Sourceport:(\\d+)\\s+TransIP\\s+:(\\S+)\\s+TransPort:(\\d+)\\s+DestIp\\s+:(\\S+)\\s+Destport:(\\d+)\\s*")
 
-		T.re2_CiscoNATSyslog=T.re2("(\\w+)\\s+(\\d+)\\s+(\\d\\d):(\\d\\d):(\\d\\d).\\d\\d\\d.*(Created|Deleted)\\s+Translation\\s+(\\w+)\\s+(\\S+):(\\S+)\\s+(\\S+):(\\S+)\\s+(\\S+):(\\S+)\\s+(\\S+):(\\S+)\\d+")
+    T.re2_CiscoNATSyslog=T.re2("(\\w+)\\s+(\\d+)\\s+(\\d\\d):(\\d\\d):(\\d\\d).\\d\\d\\d.*(Created|Deleted)\\s+Translation\\s+(\\w+)\\s+(\\S+):(\\S+)\\s+(\\S+):(\\S+)\\s+(\\S+):(\\S+)\\s+(\\S+):(\\S+)\\d+")
 
-		T.re2_CiscoNATSyslog2=T.re2("(\\w+)\\s+(\\d+)\\s+(\\d\\d):(\\d\\d):(\\d\\d).\\d\\d\\d.*(Created|Deleted)\\s+(\\w+)\\s+(\\S+):(\\d+)\\s+(\\S+):(\\d+)\\s+(\\S+):(\\d+)\\s+(\\S+):(\\d+)")
+    T.re2_CiscoNATSyslog2=T.re2("(\\w+)\\s+(\\d+)\\s+(\\d\\d):(\\d\\d):(\\d\\d).\\d\\d\\d.*(Created|Deleted)\\s+(\\w+)\\s+(\\S+):(\\d+)\\s+(\\S+):(\\d+)\\s+(\\S+):(\\d+)\\s+(\\S+):(\\d+)")
 
   end,
 
@@ -43,160 +43,161 @@ TrisulPlugin = {
 
   simplecounter = {
 
-	-- to UDP>SYSLOG protocol 
+  -- to UDP>SYSLOG protocol 
     protocol_guid = "{4323003E-D060-440B-CA26-E146C0C7DB4E}", 
 
     onpacket = function(engine,layer)
-			local syslogstr = layer:rawbytes():tostring()
+      local syslogstr = layer:rawbytes():tostring()
 
-			-- engine:add_resource( "{7B431613-9291-49BF-F8D3-73578A445310}", layer:packet():flowid():id(), "NAT SYSLOG", syslogstr) 
+      -- engine:add_resource( "{7B431613-9291-49BF-F8D3-73578A445310}", layer:packet():flowid():id(), "NAT SYSLOG", syslogstr) 
 
-			if syslogstr:find("NAT_ACCT",1,true) then 
-				-- JIO device 
+      if syslogstr:find("NAT_ACCT",1,true) then 
+        -- JIO device 
 
-				local bret, mon, day, h,m,s, deviceip, cmd, proto, sip, sport, tsip, tsport, dip, dport = T.re2_JioDeviceNATSyslog:partial_match_n(syslogstr)
+        local bret, mon, day, h,m,s, deviceip, cmd, proto, sip, sport, tsip, tsport, dip, dport = T.re2_JioDeviceNATSyslog:partial_match_n(syslogstr)
 
-				if bret ==false then return;  end
+        if bret ==false then return;  end
 
-				local tvsec = os.time( {
-					year = tonumber(os.date('%Y')),
-					month = MONTHNAMES[mon],
-					day = tonumber(day),
-					hour =h, min = m, sec = s
-				})
-				local fkey = Fk.toflow_format_v4( proto, sip,sport, dip, dport)
+        local tvsec = os.time( {
+          year = tonumber(os.date('%Y')),
+          month = MONTHNAMES[mon],
+          day = tonumber(day),
+          hour =h, min = m, sec = s
+        })
+        local fkey = Fk.toflow_format_v4( proto, sip,sport, dip, dport)
 
-				if cmd == "START" then
-					engine:update_flow_raw( fkey, 0, 1)
-					engine:tag_flow ( fkey, "[natip]"..tsip)
-					engine:tag_flow ( fkey, "[natport]"..tsport)
-					engine:tag_flow ( fkey, "[deviceip]"..deviceip)
-				elseif cmd == "STOP" then 
-					engine:terminate_flow ( fkey)
-				end 
+        if cmd == "START" then
+          engine:update_flow_raw( fkey, 0, 1)
+          engine:tag_flow ( fkey, "[natip]"..tsip)
+          engine:tag_flow ( fkey, "[natport]"..tsport)
+          engine:tag_flow ( fkey, "[deviceip]"..deviceip)
+        elseif cmd == "STOP" then 
+          engine:terminate_flow ( fkey)
+        end 
 
-			elseif syslogstr:find("LOG_TRANSLATION",1,true) then 
+      elseif syslogstr:find("LOG_TRANSLATION",1,true) then 
 
-				-- CISCO device 
-				local bret, mon, day, h,m,s, cmd, proto, sip, sport, tsip, tsport, dip, dport = T.re2_CiscoNATSyslog:partial_match_n(syslogstr)
+        -- CISCO device 
+        local bret, mon, day, h,m,s, cmd, proto, sip, sport, tsip, tsport, dip, dport = T.re2_CiscoNATSyslog:partial_match_n(syslogstr)
 
-				if bret ==false then return; end
+        if bret ==false then return; end
 
-				local tvsec = os.time( {
-					year = tonumber(os.date('%Y')),
-					month = MONTHNAMES[mon],
-					day = tonumber(day),
-					hour =h, min = m, sec = s
-				})
-				local fkey = Fk.toflow_format_v4( proto, tsip,tsport, dip, dport)
+        local tvsec = os.time( {
+          year = tonumber(os.date('%Y')),
+          month = MONTHNAMES[mon],
+          day = tonumber(day),
+          hour =h, min = m, sec = s
+        })
+        local fkey = Fk.toflow_format_v4( proto, tsip,tsport, dip, dport)
 
-				if cmd == "Created" then
-					engine:update_flow_raw( fkey, 0, 1)
-					engine:tag_flow ( fkey, "[natip]"..sip)
-					engine:tag_flow ( fkey, "[natport]"..sport)
-					engine:tag_flow ( fkey, "[deviceip]"..sip)
-
-
-				elseif cmd == "Deleted" then 
-					engine:update_flow_raw( fkey, 1, 1)
-					engine:terminate_flow ( fkey)
-				end 
-
-			elseif syslogstr:find("%IPNAT-6-NAT",1,true) then 
-
-				-- CISCO device 2 
-				local bret, mon, day, h,m,s, cmd, proto, sip, sport, tsip, tsport, dip, dport = T.re2_CiscoNATSyslog2:partial_match_n(syslogstr)
-
-				if bret ==false then return; end
-
-				local tvsec = os.time( {
-					year = tonumber(os.date('%Y')),
-					month = MONTHNAMES[mon],
-					day = tonumber(day),
-					hour =h, min = m, sec = s
-				})
-				local fkey = Fk.toflow_format_v4( proto, sip,sport, dip, dport)
-
-				if cmd == "Created" then
-					engine:update_flow_raw( fkey, 0, 1)
-					engine:tag_flow ( fkey, "[natip]"..tsip)
-					engine:tag_flow ( fkey, "[natport]"..tsport)
-					engine:tag_flow ( fkey, "[deviceip]"..sip)
-				elseif cmd == "Deleted" then 
-					engine:update_flow_raw( fkey, 1, 1)
-					engine:terminate_flow ( fkey)
-				end 
-
-			elseif syslogstr:find("TRIG=",1,true) then 
-
-				-- NetElastic BNG 
-				local   bret,
-						adddel,
-						_,
-						proto,
-						_,
-						ipversion,
-						username,
-						sip,
-						dip,
-						sport,
-						dport,
-						_,
-						natip,
-						_,
-						natsport,
-						_,
-						year,
-						mon,
-						day,
-						h,m,s= T.re2_NetElasticBNGNATSyslog:partial_match_n(syslogstr)
+        if cmd == "Created" then
+          engine:update_flow_raw( fkey, 0, 1)
+          engine:tag_flow ( fkey, "[natip]"..sip)
+          engine:tag_flow ( fkey, "[natport]"..sport)
+          engine:tag_flow ( fkey, "[deviceip]"..sip)
 
 
-				if bret ==false then return; end
+        elseif cmd == "Deleted" then 
+          engine:update_flow_raw( fkey, 1, 1)
+          engine:terminate_flow ( fkey)
+        end 
 
-				local tvsec = os.time( {
-					year =  tonumber(year),
-					month = tonumber(mon),
-					day   = tonumber(day),
-					hour =h, min = m, sec = s
-				})
-				local fkey = Fk.toflow_format_v4( proto, natip,sport, dip, dport)
+      elseif syslogstr:find("%IPNAT-6-NAT",1,true) then 
 
-				-- print(tvsec.." = "..fkey) 
+        -- CISCO device 2 
+        local bret, mon, day, h,m,s, cmd, proto, sip, sport, tsip, tsport, dip, dport = T.re2_CiscoNATSyslog2:partial_match_n(syslogstr)
 
-				if adddel == "SADD" then
-					engine:update_flow_raw( fkey, 0, 1)
-					engine:tag_flow ( fkey, "[natip]"..sip)
-					engine:tag_flow ( fkey, "[natport]"..natsport)
-					engine:tag_flow ( fkey, "[username]"..username)
-				elseif cmd == "SDEL" then 
-					engine:update_flow_raw( fkey, 1, 1)
-					engine:terminate_flow ( fkey)
-				end 
-			elseif syslogstr:find(":<Session",1,true) then 
-				-- Huawei device 
-				local   bret,
-						adddel,
-						stvsec,
-						etvsec,
-						sip,
-						natip,
-						sport,
-						dip,
-						dport,
-						proto= T.re2_HuaweiNATSyslog:partial_match_n(syslogstr)
-				if bret ==false then return; end
+        if bret ==false then return; end
 
-				local fkey = Fk.toflow_format_v4( proto, natip,sport, dip, dport)
-				if adddel == "SessionA" then
-					engine:update_flow_raw( fkey, 0, 1)
-					engine:tag_flow ( fkey, "[natip]"..sip)
-				elseif cmd == "SessionW" then 
-					engine:update_flow_raw( fkey, 1, 1)
-					engine:terminate_flow ( fkey)
-				end 
+        local tvsec = os.time( {
+          year = tonumber(os.date('%Y')),
+          month = MONTHNAMES[mon],
+          day = tonumber(day),
+          hour =h, min = m, sec = s
+        })
+        local fkey = Fk.toflow_format_v4( proto, sip,sport, dip, dport)
 
-			end 
+        if cmd == "Created" then
+          engine:update_flow_raw( fkey, 0, 1)
+          engine:tag_flow ( fkey, "[natip]"..tsip)
+          engine:tag_flow ( fkey, "[natport]"..tsport)
+          engine:tag_flow ( fkey, "[deviceip]"..sip)
+        elseif cmd == "Deleted" then 
+          engine:update_flow_raw( fkey, 1, 1)
+          engine:terminate_flow ( fkey)
+        end 
+
+      elseif syslogstr:find("TRIG=",1,true) then 
+
+        -- NetElastic BNG 
+        local   bret,
+            adddel,
+            _,
+            proto,
+            _,
+            ipversion,
+            username,
+            sip,
+            dip,
+            sport,
+            dport,
+            _,
+            natip,
+            _,
+            natsport,
+            _,
+            year,
+            mon,
+            day,
+            h,m,s= T.re2_NetElasticBNGNATSyslog:partial_match_n(syslogstr)
+
+
+        if bret ==false then return; end
+
+        local tvsec = os.time( {
+          year =  tonumber(year),
+          month = tonumber(mon),
+          day   = tonumber(day),
+          hour =h, min = m, sec = s
+        })
+        local fkey = Fk.toflow_format_v4( proto, natip,sport, dip, dport)
+
+        if adddel == "SADD" then
+          engine:update_flow_raw( fkey, 0, 1)
+          engine:tag_flow ( fkey, "[natip]"..sip)
+          engine:tag_flow ( fkey, "[natport]"..natsport)
+          engine:tag_flow ( fkey, "[username]"..username)
+          engine:tag_flow ( fkey, "[addts]"..tvsec)
+        elseif adddel == "SDEL" then 
+          engine:update_flow_raw( fkey, 1, 1)
+          engine:tag_flow ( fkey, "[delts]"..tvsec)
+          engine:terminate_flow ( fkey)
+        end 
+
+      elseif syslogstr:find(":<Session",1,true) then 
+        -- Huawei device 
+        local   bret,
+            adddel,
+            stvsec,
+            etvsec,
+            sip,
+            natip,
+            sport,
+            dip,
+            dport,
+            proto= T.re2_HuaweiNATSyslog:partial_match_n(syslogstr)
+        if bret ==false then return; end
+
+        local fkey = Fk.toflow_format_v4( proto, natip,sport, dip, dport)
+        if adddel == "SessionA" then
+          engine:update_flow_raw( fkey, 0, 1)
+          engine:tag_flow ( fkey, "[natip]"..sip)
+        elseif adddel == "SessionW" then 
+          engine:update_flow_raw( fkey, 1, 1)
+          engine:terminate_flow ( fkey)
+        end 
+
+      end 
     end,
   },
 }
