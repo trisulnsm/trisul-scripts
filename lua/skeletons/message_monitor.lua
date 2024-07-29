537792501cbef6c37f919c230c5141365675a6e6
vivek <vivek@unleashnetworks.com>
2024-07-29 17:11:25 +0530

    message monitor skeleton update

--- lua/skeletons/message_monitor.lua
+++ lua/skeletons/message_monitor.lua
@@ -16,13 +16,45 @@ TrisulPlugin = {
   },
 
 
+
+  -- common functions onload, onunload, onmessage()..
+
+  -- WHEN CALLED : your LUA script is loaded into Trisul 
+  onload = function()
+    -- your code 
+  end,
+
+  -- WHEN CALLED : your LUA script is unloaded  / detached from Trisul 
+  onunload = function()
+    -- your code 
+  end,
+
+  -- any messages you want to handle for state management 
+  message_subscriptions = {},
+
+  -- WHEN CALLED: when another plugin sends you a message 
+  onmessage = function(msgid, msg)
+    -- your code 
+  end,
+
+
   -- messagemonitor  block
   -- 
   messagemonitor   = {
 
-  onflowmetric  = function(engine,flowid,meter,value)
 
-  end,
+    -- when a new flow metric is seen
+    -- used with TCP reassembly 
+    onflowmetric  = function(engine,flowid,meter,value)
+
+    end,
+
+    -- when a new NetFlow record is seen
+    -- use this to tap into NetFlow records and add counters within context of those records 
+    onnewflowrecord = function(engine, flowid, bytes_az, bytes_za, packets_az, packets_za)
+
+    end, 
+
 
   },
 }
